950c7fb27abeb43d2fa3383dbccc19490f41e84e
Nada Warsame <missnadawarsame10@gmail.com>
2026-09-12 15:23:21 +0000

    changes

--- assets/js/anime.js
+++ assets/js/anime.js
@@ -1,18 +1,20 @@
-anime({
-    targets: ".welcome-block",
-    translateY: [
-        { value: 0, duration: 300 },
-        { value: -18, duration: 450 },
-        { value: 0, duration: 500 },
-        { value: -8, duration: 250 },
-        { value: 0, duration: 350 },
-    ],
-    easing: "easeOutBounce",
-    delay: function (el, i, l) {
-        return i * 180;
-    },
-    loop: false,
-});
+if (typeof anime === "function") {
+    anime({
+        targets: ".welcome-block",
+        translateY: [
+            { value: 0, duration: 300 },
+            { value: -18, duration: 450 },
+            { value: 0, duration: 500 },
+            { value: -8, duration: 250 },
+            { value: 0, duration: 350 },
+        ],
+        easing: "easeOutBounce",
+        delay: function (el, i, l) {
+            return i * 180;
+        },
+        loop: false,
+    });
+}
 
 const timelineItems = document.querySelectorAll(".timeline-item");
 
